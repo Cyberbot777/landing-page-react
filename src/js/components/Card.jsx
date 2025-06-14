06d309d7b1b0025cde8de0cacb6ac03a2ac51ab5
Cyberbot777 <Cyberbot777@users.noreply.github.com>
2025-06-13 19:51:07 -0700

    Added footer info

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Card = ({ image, title, text, buttonLabel }) => {
 	return (
-		<div className="card h-100">
+		<div className="card h-100 bg-dark text-white border-light">
 			<img src={image} className="card-img-top" alt={title} />
 			<div className="card-body d-flex flex-column justify-content-between">
 				<h5 className="card-title">{title}</h5>
 				<p className="card-text">{text}</p>
-				<a href="#" className="btn btn-primary mt-auto">{buttonLabel}</a>
+				<a href="#" className="btn btn-outline-light mt-auto">{buttonLabel}</a>
 			</div>
 		</div>
 	);
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,46 +1,59 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
 import Jumbotron from "./Jumbotron.jsx";
-import Card from "./Card.jsx";
-import Footer from "./Footer.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 const Home = () => {
-  return (
-    <>
-      <Navbar />
-      <Jumbotron />
+	const cardData = [
+		{
+			image: "https://via.placeholder.com/500x325",
+			title: "Card 1",
+			text: "This is the first card's text.",
+			buttonLabel: "Find Out More!"
+		},
+		{
+			image: "https://via.placeholder.com/500x325",
+			title: "Card 2",
+			text: "Card 2 has its own custom content.",
+			buttonLabel: "Find Out More!"
+		},
+		{
+			image: "https://via.placeholder.com/500x325",
+			title: "Card 3",
+			text: "Third card here, still passed by props.",
+			buttonLabel: "Find Out More!"
+		},
+		{
+			image: "https://via.placeholder.com/500x325",
+			title: "Card 4",
+			text: "Fourth card to round it out.",
+			buttonLabel: "Find Out More!"
+		}
 
-      <div className="container my-5">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-          <Card
-            image="https://via.placeholder.com/500x325"
-            title="Card 1"
-            text="Some quick example text for card 1."
-            buttonLabel="Find Out More!"
-          />
-          <Card
-            image="https://via.placeholder.com/500x325"
-            title="Card 2"
-            text="Some quick example text for card 2."
-            buttonLabel="Find Out More!"
-          />
-          <Card
-            image="https://via.placeholder.com/500x325"
-            title="Card 3"
-            text="Some quick example text for card 3."
-            buttonLabel="Find Out More!"
-          />
-          <Card
-            image="https://via.placeholder.com/500x325"
-            title="Card 4"
-            text="Some quick example text for card 4."
-            buttonLabel="Find Out More!"
-          />
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+		
+	];
+
+	return (
+		<>
+			<Navbar />
+			<Jumbotron />
+			<div className="container my-5">
+				<div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+					{cardData.map((card, index) => (
+						<Card
+							key={index}
+							image={card.image}
+							title={card.title}
+							text={card.text}
+							buttonLabel={card.buttonLabel}
+						/>
+					))}
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
